d19dc8cea29f6045effc8b797cb2c892cf715317
323413492 <323413492@C-H63W3V1.tdsb.on.ca>
2016-03-02 14:44:51 -0500

    Created hello world

---
+++ ICS3U/src/seravalle/HelloWorld.java
@@ -0,0 +1,19 @@
+package seravalle;
+/* HelloWorld.java
+ * Prints hello world to the console
+ * Adam Seravalle
+ * mar/02/2016
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Print "Hello World" to the console
+       System.out.println("Hello World!!!!!!!!!!!!!!!!");
+       
+       
+       
+
+	}
+
+}
+						
