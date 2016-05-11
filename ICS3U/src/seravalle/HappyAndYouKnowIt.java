8aecb97c871763c8ec2a6ca172c053e05edc64f5
323413492 <323413492@C-H63W3V1.tdsb.on.ca>
2016-05-11 14:30:37 -0400

    plays happy and you know it

--- ICS3U/src/seravalle/HappyAndYouKnowIt.java
+++ ICS3U/src/seravalle/HappyAndYouKnowIt.java
@@ -14,6 +14,38 @@ public class HappyAndYouKnowIt {
 
 	public static void main(String[] args) {
 		
+
+	
+	
+	
+		//array
+		 	String [] lyrics = new String[]{"clap your hands", "tap your toes","and you really really want to show it.", "tap your feet", "stomp your feet", "snap your fingers", "and you really wanna show it", "slap your arm", "tap your head", "tap your nose"};
+		 	
+		 		for (int i = 0; i !=10; i++) //loop for the lyrics 
+		 	{
+		 			System.out.println("If you're happy & you know it " + lyrics[i]);//displays the lyrics 
+		 		}
+		 		
+		 	}
+		 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 	
@@ -22,4 +54,4 @@ public class HappyAndYouKnowIt {
 	
 	
 	
-}
+
