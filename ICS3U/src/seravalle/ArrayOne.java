6775eb7936b48838b3306c03846bf6cd377bdebe
323413492 <323413492@C-H63W3V1.tdsb.on.ca>
2016-05-11 14:12:33 -0400

    added comments

--- ICS3U/src/seravalle/ArrayOne.java
+++ ICS3U/src/seravalle/ArrayOne.java
@@ -1,3 +1,9 @@
+/* Adam Seravalle
+ * May 11 2016
+ * ArrayOne.java
+ * gets a list of names from user and displays it forwards and backwards 
+ */
+
 package seravalle;
 
 import java.util.Scanner;
@@ -6,27 +12,28 @@ public class ArrayOne {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
+		// gets number of names from user
 		System.out.println(" Please enter the number of names ");
 		int numberNames = scan.nextInt();
 		scan.nextLine();
-
+		// creates the array
 		String[] names = new String[numberNames];
-
+		// puts the names in the array
 		System.out.println("Please enter the names");
 		for (int i = 0; i < numberNames; i++) {
 
 			names[i] = scan.nextLine();
 		}
-
+		// displays the names forwards
 		for (int i = 0; i < numberNames; i++) {
 
 			System.out.println(names[i]);
-				}
-		for (int i = numberNames -1; i >= 0; i--) {
+		}
+		// diplayes the names backwards order
+		for (int i = numberNames - 1; i >= 0; i--) {
 
 			System.out.println(names[i]);
-				}
+		}
 	}
 
 }
---
+++ ICS3U/src/seravalle/HappyAndYouKnowIt.java
@@ -0,0 +1,25 @@
+/* Adam Seravalle
+ * May 11 2016
+ * HappyAndYouKnowIt.java
+ * displays the verses of if your happy and you know it  
+ */
+
+
+
+
+
+package seravalle;
+
+public class HappyAndYouKnowIt {
+
+	public static void main(String[] args) {
+		
+	}
+
+	
+	
+	
+	
+	
+	
+}
