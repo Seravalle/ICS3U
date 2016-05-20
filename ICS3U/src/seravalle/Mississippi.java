9f8d11d2379422c12014b4b582da14ed4f960329
323413492 <323413492@C-H63W3V1.tdsb.on.ca>
2016-05-20 14:21:39 -0400

    prints mississippi

---
+++ ICS3U/src/seravalle/Mississippi.java
@@ -0,0 +1,84 @@
+/*Adam Seravalle
+ * May 20 2016
+ * Mississippi.java
+ * Prints the letters of mississippi with the letters
+ * 
+ */
+
+
+
+
+
+
+
+package seravalle;
+
+public class Mississippi {
+
+	// method for printing M
+	public static void printM(){
+		System.out.println("M           M");
+		System.out.println("M M       M M");
+		System.out.println("M  M     M  M");
+		System.out.println("M   M   M   M");
+		System.out.println("M    M M    M");
+		System.out.println("M     M     M");
+		 System.out.println("");
+	
+	}
+	// method for printing I
+	public static void printI(){
+		
+		System.out.println("IIIIIIIII");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("    I");
+		System.out.println("IIIIIIIII");
+		 System.out.println("");
+	}
+	// method for printing S
+ public static void printS(){
+	 
+
+	 System.out.println(" SSSSS ");
+	 System.out.println("S    S");
+	 System.out.println("S ");
+	 System.out.println(" SSSSS");
+	 System.out.println("      S ");
+	 System.out.println( "S    S ");
+	 System.out.println(" SSSSS ");
+	 System.out.println("");
+ }
+	// method for printing P
+public static void printP(){
+	 System.out.println("PPPPPPPP");
+	 System.out.println("P      PP");
+	 System.out.println("PPPPPPP");
+	 System.out.println("P");
+	 System.out.println("P");
+	 System.out.println("P");
+	 System.out.println("");
+}
+	
+	
+	// main method
+	public static void main(String[] args) {
+	// call method and print letters	
+printM();
+printI();
+printS();
+printS();
+printI();
+printS();
+printS();
+printI();
+printP();
+printP();
+printI();
+		
+		
+		
+	}
+
+}
